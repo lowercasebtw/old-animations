6af8bdf8e6050a10bbe56978e88c59b57111581b
lowercasebtw <jejojora2@gmail.com>
2024-04-23 22:23:58 -0400

    Update to 1.20.5

--- src/main/java/btw/lowercase/oldanimations/mixin/MixinWorldRenderer.java
+++ src/main/java/btw/lowercase/oldanimations/mixin/MixinWorldRenderer.java
@@ -29,17 +29,19 @@ public abstract class MixinWorldRenderer {
 
     @Shadow @Nullable private VertexBuffer darkSkyBuffer;
 
-    @Inject(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/VertexBuffer;bind()V", ordinal = 0))
-    private void renderSky$old$storeShader(CallbackInfo ci, @Local Vec3d vec3d, @Local ShaderProgram shaderProgram, @Share("skyVec") LocalRef<Vec3d> vecRef, @Share("shaderProgram") LocalRef<ShaderProgram> programRef) {
+    @Inject(method = "renderSky(Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/VertexBuffer;bind()V", ordinal = 0))
+    private void renderSky$old$storeShader(CallbackInfo ci, @Local MatrixStack matrixStack, @Local Vec3d vec3d, @Local ShaderProgram shaderProgram, @Share("matrixStack") LocalRef<MatrixStack> matrixStackRef, @Share("skyVec") LocalRef<Vec3d> vecRef, @Share("shaderProgram") LocalRef<ShaderProgram> programRef) {
+        matrixStackRef.set(matrixStack);
         vecRef.set(vec3d);
         programRef.set(shaderProgram);
     }
 
-    @Inject(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
-            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderColor(FFFF)V", ordinal = 6))
-    private void renderSky$old$deepBlue(CallbackInfo ci, @Local(argsOnly = true) float f, @Local(ordinal = 0, argsOnly = true) MatrixStack matrixStack, @Local(argsOnly = true) Matrix4f matrix4f, @Share("skyVec") LocalRef<Vec3d> vecRef, @Share("shaderProgram") LocalRef<ShaderProgram> shaderProgramLocalRef) {
+    @Inject(method = "renderSky(Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderColor(FFFF)V", ordinal = 6))
+    private void renderSky$old$deepBlue(CallbackInfo ci, @Local(ordinal = 0, argsOnly = true) Matrix4f matrix4f, @Local(ordinal = 1, argsOnly = true) Matrix4f matrix4f2, @Local(argsOnly = true) float f, @Share("matrixStack") LocalRef<MatrixStack> matrixStackRef, @Share("skyVec") LocalRef<Vec3d> vecRef, @Share("shaderProgram") LocalRef<ShaderProgram> shaderProgramLocalRef) {
         if (!OldAnimations.CONFIG.legacySettings.OLD_SKY_RENDERER) return;
+
+        MatrixStack matrixStack = matrixStackRef.get();
+
         Vec3d skyVec = vecRef.get();
         float skyColorR = (float) skyVec.x;
         float skyColorG = (float) skyVec.y;
@@ -55,14 +57,14 @@ public abstract class MixinWorldRenderer {
         // If Statement is a fix for it showing below y 0/-64
         // Not entirely accurate functionality
         assert this.client.player != null;
-        double d = this.client.player.getCameraPosVec((float)f).y - this.world.getLevelProperties().getSkyDarknessHeight(this.world);
+        double d = this.client.player.getCameraPosVec(f).y - this.world.getLevelProperties().getSkyDarknessHeight(this.world);
         if (d > 0.0) {
             matrixStack.push();
             // Changes color based on camera/player Y level
             matrixStack.translate(0.0F, -((float) (d - 16.0)), 0.0F);
             assert this.darkSkyBuffer != null;
             this.darkSkyBuffer.bind();
-            this.darkSkyBuffer.draw(matrixStack.peek().getPositionMatrix(), matrix4f, shaderProgramLocalRef.get());
+            this.darkSkyBuffer.draw(matrixStack.peek().getPositionMatrix(), matrix4f2, shaderProgramLocalRef.get());
             VertexBuffer.unbind();
             matrixStack.pop();
         }
